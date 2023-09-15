473f18c363bebb26588b9e9d9b33b2396d0b8fa1
Nolan Nguyen! <nolan2087@gmail.com>
2023-09-15 13:52:17 -0700

    fixed convertToNATO.cgit add convertToNATO.c

--- classwork/classwork03/convertToNATO.c
+++ classwork/classwork03/convertToNATO.c
@@ -8,8 +8,8 @@ int main(int argc, char * argv[] ) {
     char outputString[25] = "\0";
 
     // Arrays of letters and NATO alphabet.
-    char *letters[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    char *nato[] = {"Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu"};
+    char letters[26] = {"abcdefghijklmnopqrstuvwxyz"};
+    char nato[26][10] = {"Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu"};
 
     // User input.
     char entry[25] = "";
@@ -36,20 +36,21 @@ int main(int argc, char * argv[] ) {
     // Nested loops: loops through the user input letter by letter, and loops through letters[]
     // and compares them. If they're the same, it concatenates it to an output string 
     // and goes to the next line.
-    for (int i = 0; i < 26; i++) {
-        printf("Checking i: %c \n", entry[i]);
+    for (int i = 0; i < sizeof(entry); i++) {
+        // printf("Checking i: %c \n", entry[i]);
 
-        for (int k = 0; k < 26; k++) {
-            printf("Checking k: %c \n", letters[k]);
+        for (int k = 0; k < sizeof(letters); k++) {
+            // printf("Checking k: %c \n", letters[k]);
 
             if (entry[i] == letters[k]) {
-                    printf("Success! Letter found.");
-                    strcat(nato[k], outputString);
-                    strcat("\n", outputString);
+                    // printf("Success! Letter found.\n");
+                    // strcat(nato[k], outputString);
+                    // strcat("\n", outputString);
+                    printf("%s \n", nato[k]);
             }
         }
     }
 
-    printf("%s", outputString);
+    // printf("%s", outputString);
 
 }
