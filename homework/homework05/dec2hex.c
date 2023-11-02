64d242d2dc5857bce76753a2bcecc633ea8a3549
Nolan Nguyen! <nolan2087@gmail.com>
2023-11-02 10:08:03 -0700

    adding progress on dec2hex.c

--- homework/homework05/dec2hex.c
+++ homework/homework05/dec2hex.c
@@ -9,6 +9,9 @@ void convertToHex(int decimal, int numbits) {
     }
 
     int binEquivalent[numbits];
+    int bytesum;
+    char* hexEquivalent[numbits / 4];
+    int hexindex = 0;
 
     for (int i=0; i<numbits; i++) {
         binEquivalent[i] = 0;
@@ -25,11 +28,44 @@ void convertToHex(int decimal, int numbits) {
         two_divisor = two_divisor / 2;
     }
     // Now we have a binary number stored in binEquivalent, and we just need to
-    // four bits at a time into HEX.
+    // convert four bits at a time into HEX.
+    for (int bit = 0; bit < numbits; bit = bit + 4) {
 
+        bytesum = 8*binEquivalent[bit] + 4*binEquivalent[bit+1] + 2*binEquivalent[bit+2] + binEquivalent[bit+3];
+        if (bytesum > 9) {
+            switch (bytesum)
+            {
+            case 10:
+                hexEquivalent[hexindex] = "A";
+                break;
+            case 11:
+                hexEquivalent[hexindex] = "B";
+                break;
+            case 12:
+                hexEquivalent[hexindex] = "C";
+                break;
+            case 13:
+                hexEquivalent[hexindex] = "D";
+                break;
+            case 14:
+                hexEquivalent[hexindex] = "E";
+                break;  
+            case 15:
+                hexEquivalent[hexindex] = "F"; 
+            
+            default:
+                printf("... How did this happen???");
+                break;
+            }
+        }
+        else {
+            hexEquivalent[hexindex] = bytesum;
+        }
+        hexindex++;
+    }
 
-    for (int i = 0; i < numbits; i++) {
-        printf("%d", binEquivalent[i]);
+    for (int i = 0; i < numbits / 4; i++) {
+        printf("Digit: %c\n", hexEquivalent[i]);
     }
     
 }
@@ -46,3 +82,4 @@ int main(int argc, char * argv[]) {
     convertToHex(decimalVersion, bitcount);
     return 0;
 }
+
