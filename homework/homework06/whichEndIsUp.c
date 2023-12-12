b3617e349376d31587ca356ba403cc629487afb5
B.J. Johnson <bjohnson05@gmail.com>
2023-12-11 20:50:29 -0800

    Added homework06 evaluation

--- homework/homework06/whichEndIsUp.c
+++ homework/homework06/whichEndIsUp.c
@@ -10,9 +10,9 @@ int main() {
     int num = 1;  // 4-byte integer with a value of 1 (00000000 00000000 00000000 00000001)
 
     // Use a pointer to examine the first byte
-    int *bytePtr = (unsigned char *) & num;
+    int *bytePtr = (unsigned char *) &num;
 
-    if (* bytePtr == 1) {
+    if (*bytePtr == 1) {
         printf("Your computer is Little-Endian.\n");
     } else {
         printf("Your computer is Big-Endian.\n");
